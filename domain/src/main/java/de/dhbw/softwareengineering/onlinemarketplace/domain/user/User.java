a5bd0e1117e6a36b8a8b9af1c7cf98b9d1a33bb8
WeberWr <97832247+WeberWr@users.noreply.github.com>
2024-04-16 16:54:37 +0200

    add validation

--- domain/src/main/java/de/dhbw/softwareengineering/onlinemarketplace/domain/user/User.java
+++ domain/src/main/java/de/dhbw/softwareengineering/onlinemarketplace/domain/user/User.java
@@ -18,6 +18,7 @@ public final class User {
         Validate.notBlank(email);
         Validate.isTrue(EmailValidator.getInstance().isValidEmail(email));
         Validate.notBlank(password);
+
         this.id = UUID.randomUUID();
         this.name = name;
         this.email = email;
@@ -39,30 +40,4 @@ public final class User {
     public String password() {
         return password;
     }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (User) obj;
-        return Objects.equals(this.id, that.id) &&
-                Objects.equals(this.name, that.name) &&
-                Objects.equals(this.email, that.email) &&
-                Objects.equals(this.password, that.password);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, email, password);
-    }
-
-    @Override
-    public String toString() {
-        return "User[" +
-                "id=" + id + ", " +
-                "name=" + name + ", " +
-                "email=" + email + ", " +
-                "password=" + password + ']';
-    }
-
 }
--- domain/src/main/java/de/dhbw/softwareengineering/onlinemarketplace/domain/valueObject/Name.java
+++ domain/src/main/java/de/dhbw/softwareengineering/onlinemarketplace/domain/valueObject/Name.java
@@ -4,41 +4,9 @@ import org.apache.commons.lang3.Validate;
 
 import java.util.Objects;
 
-public final class Name {
-    private final String firstName;
-    private final String lastName;
-
-    public Name(String firstName, String lastName) {
+public record Name(String firstName, String lastName) {
+    public Name {
         Validate.notBlank(firstName);
         Validate.notBlank(lastName);
-        this.firstName = firstName;
-        this.lastName = lastName;
     }
-
-    public String firstName() {
-        return firstName;
-    }
-
-    public String lastName() {
-        return lastName;
-    }
-
-    @Override
-    public String toString() {
-        return firstName + " " + lastName;
-    }
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (Name) obj;
-        return Objects.equals(this.firstName, that.firstName) &&
-                Objects.equals(this.lastName, that.lastName);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(firstName, lastName);
-    }
-
 }
